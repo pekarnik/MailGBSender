0f7ed57d3968c6811a8ed5532b6fdb1dad16bf63
Pekarnik <pekgoglo@gmail.com>
2020-05-31 23:51:28 +0300

    3 Task Done

--- MailSender/WpfMailSender.xaml.cs
+++ MailSender/WpfMailSender.xaml.cs
@@ -18,9 +18,9 @@ namespace MailSender
 	/// <summary>
 	/// Логика взаимодействия для MainWindow.xaml
 	/// </summary>
-	public partial class MainWindow : Window
+	public partial class WpfMailSender : Window
 	{
-		public MainWindow()
+		public WpfMailSender()
 		{
 			InitializeComponent();
 		}
