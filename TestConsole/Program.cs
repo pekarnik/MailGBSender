05f3a1149a441648fe90e75414f48f4b303b20f4
Pekarnik <pekgoglo@gmail.com>
2020-05-31 21:10:45 +0300

    Created Project and send button

---
+++ TestConsole/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+using System.Windows;
+
+namespace TestConsole
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			MailMessage mm = new MailMessage("pekarnik11@yandex.ru",
+				"sh5@inbox.ru");
+			mm.Subject = "Заголовок письма";
+			mm.Body = "Содержимое письма";
+
+			mm.IsBodyHtml = false;
+
+			SmtpClient sc = new SmtpClient("smtp.yandex.ru", 25);
+			sc.EnableSsl = true;
+			sc.DeliveryMethod = SmtpDeliveryMethod.Network;
+			sc.UseDefaultCredentials = false;
+			Console.WriteLine("Введите пароль:");
+			string password = Console.ReadLine();
+			sc.Credentials = new NetworkCredential("pekarnik11@yandex.ru", password);
+			
+			try
+			{
+				sc.Send(mm);
+			}
+			catch(Exception ex)
+			{
+				MessageBox.Show("Невозможно отправить письмо " + ex.ToString());
+			}
+
+		}
+	}
+}
---
+++ WpfTestMailSender/WpfMailSender.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Windows;
+using System.Net;
+using System.Net.Mail;
+using System.Collections.Generic;
+
+namespace WpfTestMailSender
+{
+	/// <summary>
+	/// Логика взаимодействия для MainWindow.xaml
+	/// </summary>
+	public partial class WpfMailSender : Window
+	{
+		public WpfMailSender()
+		{
+			InitializeComponent();
+		}
+
+		private void btnSendEmail_Click(object sender, RoutedEventArgs e)
+		{
+			List<string> listStrMails = new List<string> { "sh5@inbox.ru" };
+			string strPassword = passwordBox.Password;
+			strPassword = passwordBox.Password;
+			foreach (string mail in listStrMails)
+			{
+				using (MailMessage mm = new MailMessage("pekarnik11@yandex.ru", mail))
+				{
+					mm.Subject = "Привет из C#";
+					mm.Body = "Hello, world!";
+					mm.IsBodyHtml = false;
+
+					using (SmtpClient sc = new SmtpClient("smtp.yandex.ru", 25))
+					{
+						sc.EnableSsl = true;
+						sc.Credentials = new NetworkCredential("pekarnik11@yandex.ru",
+							strPassword);
+						try
+						{
+							sc.Send(mm);
+						}
+						catch(Exception ex)
+						{
+							MessageBox.Show("Невозможно отправить письмо " +
+								ex.ToString());
+						}
+					}
+				}
+			}
+			MessageBox.Show("Работа завершена.")
+		}
+	}
+}
