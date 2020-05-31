9cb8bea230b5b79b027ba7cc7a955d9b7708ba3b
Pekarnik <pekgoglo@gmail.com>
2020-05-31 23:33:02 +0300

    1c Error window created

--- WpfTestMailSender/WpfMailSender.xaml.cs
+++ WpfTestMailSender/WpfMailSender.xaml.cs
@@ -20,33 +20,10 @@ namespace WpfTestMailSender
 			string mail = "sh5@inbox.ru";
 			string strPassword = passwordBox.Password;
 			strPassword = passwordBox.Password;
-			//using (MailMessage mm = new MailMessage(StaticVariableClass.senderMail, mail))
-			//{
-			//	mm.Subject = "Привет из C#";
-			//	mm.Body = "Hello, world!";
-			//	mm.IsBodyHtml = false;
-
-			//	using (SmtpClient sc = new SmtpClient(StaticVariableClass.SmtpServer, 
-			//		StaticVariableClass.SmtpPort))
-			//	{
-			//		sc.EnableSsl = true;
-			//		sc.Credentials = new NetworkCredential(StaticVariableClass.senderMail,
-			//			strPassword);
-			//		try
-			//		{
-			//			sc.Send(mm);
-			//		}
-			//		catch(Exception ex)
-			//		{
-			//			MessageBox.Show("Невозможно отправить письмо " +
-			//				ex.ToString());
-			//		}
-			//	}
-			//}
 
 			EmailSendServiceClass emailSender = new EmailSendServiceClass(StaticVariableClass.senderMail, strPassword, mail,
 				"Привет из C#", "Hello, world!", false, StaticVariableClass.SmtpServer, StaticVariableClass.SmtpPort,
-				true, new SendEndWindow(), new SendEndWindow());
+				true, new SendEndWindow(), new SendErrorWindow());
 			emailSender.Send();
 		}
 	}
